d8f7fbf938f0c75098dc11f6af1bb2bc0a3ed593
unknown <novinda@bsa.id.com>
2022-03-09 21:22:58 +0800

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'LibraryFE',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  router: {
+     middleware: ['auth']
+  },
+  auth: {
+    cookie: { options: { secure: process.env.NODE_ENV == 'production' } },
+    redirect: {
+      login: '/login',
+      logout: '/',
+      callback: '/login',
+      home: '/default'
+    },
+    strategies: {
+      local: {
+        token: {
+          property: 'token',
+          global: true,
+          // required: true,
+          // type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: '/auth/login', method: 'post' },
+          logout: { url: '/auth/logout', method: 'post' },
+          user: { url: '/auth/email/request', method: 'get' }
+        }
+      }
+    }
+  },
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: 'http://localhost:8055',
+  },
+}
---
+++ pages/login.vue
@@ -0,0 +1,54 @@
+<template>
+<b-container fluid="md" class="mt-5 mb-5">
+    <b-row>
+      <b-col md="12">
+        <b-card class="shadow-md border-0 rounded-lg">
+           <h2 class="title is-3 has-text-grey">
+      Login 
+    </h2>
+    <section>
+        <b-field label="Email">
+            <b-input name ='email' v-model="login.username" type="email"  maxlength="30" >
+            </b-input>
+        </b-field>
+
+        <b-field label="Password">
+            <b-input name= 'password' v-model="login.password" type="password" maxlength="30" ></b-input>
+        </b-field>
+       <b-button type="submit" variant="primary" @click="userLogin()">Log in</b-button>
+        
+    </section>
+    </b-card>
+     </b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+export default {
+    name :"LoginPage",
+  data() {
+    return {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async userLogin() {
+      try {
+        const response = await this.$auth.loginWith('http://localhost:8055/auth/login', { data: this.login })
+        console.log(response)
+       
+        
+    //     this.$auth.strategy.token.status().valid()
+    //   window.location = 'http://localhost:3000/index'
+        // })
+      
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
